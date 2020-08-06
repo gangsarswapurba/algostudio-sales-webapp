85f686f14e11cf8c696d626af69eadedd359418a
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-07 05:34:47 +0700

    dummy values for chart #2

--- application/views/_partials/scripts.php
+++ application/views/_partials/scripts.php
@@ -21,23 +21,28 @@ function getRandomInt(max) {
 
 var amount_orders = [];
 var to_date = new Date().getDate();
+var days_in_month = new Date(new Date().getYear(), new Date().getMonth(), 0).getDate();
+var days_label = []
 
-for (var i = 0; i < 31; i++) {
+for (var i = 0; i < days_in_month; i++) {
   if (i < to_date) {
     amount_orders[i] = getRandomInt(10);
   } else {
     amount_orders[i] = 0;
   }
+}
 
+for (var j = 1; j <= days_in_month; j++) {
+  days_label.push(j);
 }
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
+        labels: days_label,
         datasets: [{
-            label: 'Penjualan tanggal #',
+            label: 'Penjualan tanggal ke #',
             data: amount_orders,
             borderWidth: 1,
             backgroundColor: '#71c7ec'
