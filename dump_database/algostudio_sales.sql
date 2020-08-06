d80ded27051b0acde568a7bb5452ac59d85b470e
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-06 15:09:26 +0700

    export database

--- dump_database/algostudio_sales.sql
+++ dump_database/algostudio_sales.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 29-Jul-2019 às 07:04
--- Versão do servidor: 10.3.16-MariaDB
--- versão do PHP: 7.3.7
+-- Host: localhost
+-- Waktu pembuatan: 06 Agu 2020 pada 10.06
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,23 +19,36 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `product_order`
+-- Database: `algostudio`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `order`
+-- Struktur dari tabel `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order`
 --
 
 CREATE TABLE `order` (
   `id` int(11) NOT NULL,
   `data` datetime NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1
+  `status` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `order`
+-- Dumping data untuk tabel `order`
 --
 
 INSERT INTO `order` (`id`, `data`, `status`) VALUES
@@ -44,63 +57,61 @@ INSERT INTO `order` (`id`, `data`, `status`) VALUES
 (10, '2019-07-28 23:52:10', 0),
 (11, '2019-07-28 23:52:15', 0),
 (12, '2019-07-28 23:52:19', 0),
-(13, '2019-07-29 00:59:07', 1),
+(13, '2019-07-29 00:59:07', 0),
 (14, '2019-07-29 00:59:34', 1),
 (15, '2019-07-29 00:59:41', 1),
-(16, '2019-07-29 01:48:39', 0);
+(16, '2019-07-29 01:48:39', 0),
+(17, '2020-08-06 12:41:03', 1),
+(18, '2020-08-06 13:08:23', 1),
+(19, '2020-08-06 13:10:42', 1),
+(20, '2020-08-06 13:11:02', 1),
+(21, '2020-08-06 13:12:11', 1),
+(22, '2020-08-06 13:13:44', 1),
+(23, '2020-08-06 13:13:49', 1),
+(24, '2020-08-06 14:34:16', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `product`
+-- Struktur dari tabel `product`
 --
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `sku` varchar(45) NOT NULL,
   `nome` varchar(90) NOT NULL,
-  `descricao` longtext DEFAULT NULL,
+  `descricao` longtext,
   `preco` decimal(10,2) NOT NULL,
   `status` varchar(45) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `product`
+-- Dumping data untuk tabel `product`
 --
 
 INSERT INTO `product` (`id`, `sku`, `nome`, `descricao`, `preco`, `status`) VALUES
-(4, 'Sku', 'Nome', '1', '10.00', '0'),
-(5, 'tee', 'tste', '<p>12</p>', '12.00', '0'),
-(6, 'ss', 'form', '<p>tesete</p>', '12.00', '0'),
-(7, 'a', 'a', '<p>a</p>', '1.00', '0'),
-(8, 'as', 'as', '<p><b>as</b></p>', '1.00', '0'),
-(9, 'botao', 'novo', '<p>sdad</p>', '123213.00', '0'),
-(10, 'ex', 'ex', 'ex', '2.00', '0'),
-(11, 'novos', 'novos', '<p>aaa</p>', '12.00', '0'),
-(12, '100010', 'Camiseta', '<p>Camiseta vermelha</p>', '50.00', '0'),
-(13, 'Gol G2 96 1.0', 'Carro', '<p>Motor CHT</p>', '5000.00', '0'),
-(14, '19022', 'Tenis Adidas', '<p><b><span style=\"font-size: 36px;\">Bonitao</span></b><span style=\"font-size: 36px;\">﻿</span></p>', '212.90', '0'),
-(15, '001', 'Produto 1', '<p><b>Produto - 1: </b>Descriptions<br></p>', '10.00', '1'),
-(16, '002', 'Produto 2', '', '13.90', '1'),
-(17, '003', 'Produto 3', '', '17.23', '1'),
-(18, '004', 'Produto 4', '', '5.15', '1'),
-(19, 'excluir', 'Produto 5', '', '12.00', '0');
+(15, '001', 'Lenovo 1', '', '21.00', '1'),
+(16, '002', 'Lenovo 2', '', '13.90', '1'),
+(17, '003', 'Lenovo 3', '', '17.23', '1'),
+(18, '004', 'Lenovo 4', '', '5.15', '1'),
+(19, '005', 'Lenovo 5', '', '12.00', '0'),
+(20, '005', 'Lenovo 5', '', '32.00', '1');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `product_order`
+-- Struktur dari tabel `product_order`
 --
 
 CREATE TABLE `product_order` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `product_qtd` int(11) NOT NULL DEFAULT 1
+  `product_qtd` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `product_order`
+-- Dumping data untuk tabel `product_order`
 --
 
 INSERT INTO `product_order` (`id`, `order_id`, `product_id`, `product_qtd`) VALUES
@@ -121,26 +132,36 @@ INSERT INTO `product_order` (`id`, `order_id`, `product_id`, `product_qtd`) VALU
 (16, 14, 18, 1),
 (17, 15, 15, 1),
 (18, 15, 18, 1),
-(19, 16, 19, 1);
+(19, 16, 19, 1),
+(20, 17, 15, 5),
+(21, 23, 16, 1),
+(22, 24, 16, 2),
+(23, 24, 17, 1);
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `order`
+-- Indeks untuk tabel `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD KEY `ci_sessions_timestamp` (`timestamp`);
+
+--
+-- Indeks untuk tabel `order`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `product`
+-- Indeks untuk tabel `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `product_order`
+-- Indeks untuk tabel `product_order`
 --
 ALTER TABLE `product_order`
   ADD PRIMARY KEY (`id`),
@@ -148,33 +169,33 @@ ALTER TABLE `product_order`
   ADD KEY `product_po_idx` (`product_id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT de tabela `order`
+-- AUTO_INCREMENT untuk tabel `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- AUTO_INCREMENT de tabela `product`
+-- AUTO_INCREMENT untuk tabel `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT de tabela `product_order`
+-- AUTO_INCREMENT untuk tabel `product_order`
 --
 ALTER TABLE `product_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- Restrições para despejos de tabelas
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Limitadores para a tabela `product_order`
+-- Ketidakleluasaan untuk tabel `product_order`
 --
 ALTER TABLE `product_order`
   ADD CONSTRAINT `order_po` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
