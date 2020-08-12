31d2fbf6e58759f1c96265da458a2384f130019c
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-12 22:45:16 +0700

    convert nav-item styles to button style

--- application/views/_partials/header.php
+++ application/views/_partials/header.php
@@ -6,9 +6,15 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav ml-auto">
-                <a class="nav-item nav-link" href="<?= base_url('') ?>">Dashboard</a>
-                <a class="nav-item nav-link" href="<?= base_url('/product') ?>">Produk</a>
-                <a class="nav-item nav-link" href="<?= base_url('/order') ?>">Penjualan</a>
+                <a class="nav-item nav-link" href="<?= base_url('') ?>">
+                  <button type="button" class="btn btn-light">Dashboard</button>
+                </a>
+                <a class="nav-item nav-link" href="<?= base_url('/product') ?>">
+                  <button type="button" class="btn btn-light">Produk</button>
+                </a>
+                <a class="nav-item nav-link" href="<?= base_url('/order') ?>">
+                  <button type="button" class="btn btn-light">Penjualan</button>
+                </a>
             </div>
         </div>
     </nav>
