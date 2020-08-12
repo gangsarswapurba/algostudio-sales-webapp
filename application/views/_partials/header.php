799fcb199a661f41f1e00ccf2d0db9364519b75a
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-12 21:36:55 +0700

    fix typo header nav menu "Dashboard"

--- application/views/_partials/header.php
+++ application/views/_partials/header.php
@@ -6,7 +6,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav ml-auto">
-                <a class="nav-item nav-link" href="<?= base_url('') ?>">Dashboard 1</a>
+                <a class="nav-item nav-link" href="<?= base_url('') ?>">Dashboard</a>
                 <a class="nav-item nav-link" href="<?= base_url('/product') ?>">Produk</a>
                 <a class="nav-item nav-link" href="<?= base_url('/order') ?>">Penjualan</a>
             </div>
