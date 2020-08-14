db89624e40b9ba2a4d7aba1e2c9e4f0ca81c367c
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2020-08-14 09:46:44 +0700

    add footer & copyright onto it

---
+++ application/views/_partials/footer.php
@@ -0,0 +1,12 @@
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+      <p class="font-weight-light">
+        <span>© <?php echo(date('Y')) ?> Copyright:</span>
+        <a href="https://algostudio.net/"> algostudio</a>
+    </p>
+  </div>
+  <!-- Copyright -->
+
+</footer>
